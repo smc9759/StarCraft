436e530e1d00f936d9745d220828b4a69bc270ae
smc9759 <42694805+smc9759@users.noreply.github.com>
2023-09-11 14:40:47 +0900

    new functions (attack, move -

--- StarCraft/Marine.cpp
+++ StarCraft/Marine.cpp
@@ -8,7 +8,11 @@ class Marine {
 public:
 	Marine();
 	Marine(int x, int y);
+	int attack();                       // 데미지를 리턴한다.
+	void be_attacked(int damage_earn);  // 입는 데미지
+	void move(int x, int y);            // 새로운 위치
 
+	void show_status();  // 상태를 보여준다.
 };
 Marine::Marine() {
 	hp = 50;
@@ -23,7 +27,21 @@ Marine::Marine(int x, int y) {
 	damage = 5;
 	is_dead = false;
 }
-
+void Marine::move(int x, int y) {
+	coord_x = x;
+	coord_y = y;
+}
+int Marine::attack() { return damage; }
+void Marine::be_attacked(int damage_earn) {
+	hp -= damage_earn;
+	if (hp <= 0) is_dead = true;
+}
+void Marine::show_status() {
+	std::cout << " *** Marine *** " << std::endl;
+	std::cout << " Location : ( " << coord_x << " , " << coord_y << " ) "
+		<< std::endl;
+	std::cout << " HP : " << hp << std::endl;
+}
 int main() {
 
 }
