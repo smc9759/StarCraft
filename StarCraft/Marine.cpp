a8cc5b0ecb94cb2df9dd4bf4c97865ffec57fe53
smc9759 <42694805+smc9759@users.noreply.github.com>
2023-09-11 14:39:47 +0900

    constructor, main entry point

--- StarCraft/Marine.cpp
+++ StarCraft/Marine.cpp
@@ -8,4 +8,22 @@ class Marine {
 public:
 	Marine();
 	Marine(int x, int y);
+
 };
+Marine::Marine() {
+	hp = 50;
+	coord_x = coord_y = 0;
+	damage = 5;
+	is_dead = false;
+}
+Marine::Marine(int x, int y) {
+	coord_x = x;
+	coord_y = y;
+	hp = 50;
+	damage = 5;
+	is_dead = false;
+}
+
+int main() {
+
+}
